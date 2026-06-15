4c98f9c1184281545ca9aa84fbc7b5c3bc6823aa
Muhammad Anas Qadri <161422002+Anas-Rajput12@users.noreply.github.com>
2026-06-15 13:41:18 +0500

    Update page.tsx

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { motion } from 'framer-motion';
 import { Download, Mail, Globe, FileText, FileDown } from 'lucide-react';
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
